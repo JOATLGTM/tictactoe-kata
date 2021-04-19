0e6055cba37d3ca5b53f8e199eb56a2d75ff000f
JOAT <joat@joats-imac-pro.lan>
2021-04-19 14:53:12 -0400

    refactored game test script

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -4,29 +4,26 @@ import Game from './Game'
 import Board from '../Board/Board'
 
 describe('Game component', () => {
-    it('renders component', () => {
-        const component = shallow(<Game />)
-        expect(component).toHaveLength(1)
+
+    let component
+    beforeEach(() => {
+        component = mount(<Game />)
     })
 
     it('renders Board component', () => {
-        const component = mount(<Game />)
         expect(component.containsMatchingElement(<Board />)).toEqual(true)
     })
 
     it(`should prompt the player`, () => {
-        const component = mount(<Game />)
         expect(component.find('.player-prompt').text()).toBe(`Player one's turn`)
     })
 
     it('should keep track of whose turn it is', () => {
-        const component = mount(<Game />)
         component.find('button').first().simulate('click')
         expect(component.find('.player-prompt').text()).toBe(`Player two's turn`)
     });
 
     it(`should render 'X' when clicked and 'O' on second click`, () => {
-        const component = mount(<Game />)
         component.find('button').first().simulate('click')
         expect(component.find('button').first().text()).toBe('X')
         component.find('button').at(2).simulate('click')
@@ -34,7 +31,6 @@ describe('Game component', () => {
     })
 
     it('should tell you if someone won', () => {
-        const component = mount(<Game />)
         const moveSequence = [0, 3, 1, 4, 2];
     
         moveSequence.forEach(index => {
@@ -45,7 +41,6 @@ describe('Game component', () => {
     });
 
     it('should tell you if its a draw', () => {
-        const component = mount(<Game />)
         const moveSequence = [0, 1, 2, 6, 7, 8, 3, 4, 5];
     
         moveSequence.forEach(index => {
@@ -56,7 +51,6 @@ describe('Game component', () => {
     });
 
     it('should rewind when you click the rewind button', () => {
-        const component = mount(<Game />)
         component.find('button').first().simulate('click');
         expect(component.find('button').first().text()).toBe('X');
         component.find('.prevBtn').simulate('click');
@@ -64,7 +58,6 @@ describe('Game component', () => {
     });
 
     it('should reset state when you click the reset button', () => {
-        const component = mount(<Game />)
         const moveSequence = [0, 1];
 
         moveSequence.forEach(index => {
