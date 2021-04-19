26171747f57291462cf00e9e0180eb8088391517
JOAT <joat@joats-imac-pro.lan>
2021-04-19 11:19:58 -0400

    test case: render Board component in Game component

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import Game from './Game'
+import Board from '../Board/Board'
 
 describe('Game component', () => {
     it('renders component', () => {
         const component = shallow(<Game />)
         expect(component).toHaveLength(1)
+    })
+
+    it('renders Board component', () => {
+        const component = mount(<Game />)
+        expect(component.containsMatchingElement(<Board />)).toEqual(true)
     })
 })
