73e5550e35038eb3e25507f4994efcf6e6ce6605
JOAT <joat@joats-imac-pro.lan>
2021-04-18 22:40:07 -0400

    refactor test case to avoid DRY

--- src/components/Square/Square.test.js
+++ src/components/Square/Square.test.js
@@ -3,37 +3,40 @@ import { shallow, mount } from 'enzyme'
 import Square from './Square'
 
 describe('Square component', () => {
+    let component;
+    let mockCallBack;
+    beforeEach(() => {
+        mockCallBack = jest.fn()
+        component = shallow(<Square onClick={mockCallBack}/>)
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+
     it('renders component', () => {
-        const component = shallow(<Square />)
         expect(component).toHaveLength(1)
     })
 
     it('renders <button />', () => {
-        const component = shallow(<Square />)
         expect(component.find('button').length).toEqual(1)
     })
 
     it('button is clickable', () => {
-        let mockCallBack = jest.fn()
-        const component = shallow(<Square onClick={mockCallBack}/>)
         component.find('button').simulate('click')
         expect(mockCallBack.mock.calls.length).toEqual(1);
     })
 
     it('should default to an empty state', () => {
-        let mockCallBack = jest.fn()
-        const component = shallow(<Square onClick={mockCallBack}/>)
         expect(component.text()).toBe('')
     })
 
     it(`should render string 'X' if props passed down`, () => {
-        let mockCallBack = jest.fn()
         const component = shallow(<Square onClick={mockCallBack} value={'X'}/>)
         expect(component.text()).toBe('X')
     })
 
     it(`should render string 'O' if props passed down`, () => {
-        let mockCallBack = jest.fn()
         const component = shallow(<Square onClick={mockCallBack} value={'O'}/>)
         expect(component.text()).toBe('O')
     })
