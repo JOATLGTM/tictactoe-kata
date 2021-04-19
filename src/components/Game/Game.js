486b8ee8915a7d08a05d8f8c14d3938d7e3fe43a
JOAT <joat@joats-imac-pro.lan>
2021-04-19 13:34:31 -0400

    added logic to determine who is winner

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -16,6 +16,9 @@ export default class Game extends Component {
         const history = this.state.history;
         const current = history[history.length - 1];
         const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+          }
         squares[i] = this.state.playerOne ? 'X' : 'O';
         this.setState({
           history: history.concat([{
@@ -28,7 +31,11 @@ export default class Game extends Component {
     render() {
         const history = this.state.history;
         const current = history[history.length - 1]
-
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+          status = winner === 'X' ? 'Player One Wins!' : 'Player Two Wins!'
+        } 
         return (
             <div>
                 <Board 
@@ -36,9 +43,32 @@ export default class Game extends Component {
                     onClick={(i) => this.handleClick(i)}
                 />
                 <div className="player-prompt">
-                    {this.state.playerOne ? `Player one's` : `Player two's`} turn
+                    {winner ? '' : this.state.playerOne ? `Player one's turn` : `Player two's turn`}
+                </div>
+                <div className="result">
+                    {status}
                 </div>
             </div>
         )
     }
 }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
